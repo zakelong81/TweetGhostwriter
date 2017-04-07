1d9f941401b70c9c47794f41b24923b4641b3b02
Unknown <lnguyen34@mail.csuchico.edu>
2017-04-06 18:51:59 -0700

    fdasf

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,6 +1,18 @@
 function writeTweet()
 {
-  
+  var randIndex, sentence;
+  var intro = ["Have you ever noticed", "What's the deal with" , "Did you realize that", "Aren't"];
+  var body = ["cafeteria food", "the weather", "laptop", "android phone", "your house","your dog"];
+  var end = ["is trying to ruining your life", "is your enemy"];
+
+  randIndex = randomUpTo(intro.length - 1);
+  sentence = intro[randIndex];
+  randIndex = randomUpTo(body.length - 1);
+  sentence += " " + body[randIndex];
+  randIndex = randomUpTo(end.length - 1);
+  sentence += " " + end[randIndex];
+
+  document.getElementById("tweet").innerHTML = sentence;
 }
 
 //Generates a random whole number between 0 and max (inclusive)
